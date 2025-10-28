ff49a36a6563fae8bc316eb96d93168d591d2cbe
sakshipdp <sakshi.63001@gmail.com>
2025-10-29 01:53:11 +0530

    test3

--- read_data_api.py
+++ read_data_api.py
@@ -1,6 +1,10 @@
 import requests
 import pandas as pd 
 
+import os
+
+token = os.getenv("API_TOKEN")
+
 response = requests.get("https://jsonplaceholder.typicode.com/users")
 data = response.json()
 df = pd.DataFrame(data)
